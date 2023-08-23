fb9080ccfe92bf0d4daa69f4fea614505d90b623
Erica <ehu@terpmail.umd.edu>
2023-08-23 11:44:37 -0400

    Fix Challenge 25
    Challenge 25 has been fixed.
    
    Cleaned up some pyc files.

--- s4c25.py
+++ s4c25.py
@@ -6,11 +6,10 @@ from s3c18 import encrypt_AES_CTR
 from s1c7 import decrypt_AES_ECB_128
 
 #Ciphertext Generator
-def generate_challenge():
+def generate_challenge(chall_text : bytes):
     nonce = randint(0, 2**64 - 1)
     key = bytes([randint(0,255) for i in range(16)])
-    challenge_text = encrypt_AES_CTR(decrypt_AES_ECB_128(retrieve_data("7.txt"), bytes("YELLOW SUBMARINE","ascii")), key, nonce)
-
+    challenge_text = encrypt_AES_CTR(decrypt_AES_ECB_128(chall_text, bytes("YELLOW SUBMARINE","ascii")), key, nonce)
     return (challenge_text, (lambda x: encrypt_AES_CTR(x, key, nonce)))
 
 def edit_plain(base, edit, offset):
@@ -25,14 +24,17 @@ def attack(ciphertext, oracle):
 
 #Challenge Code
 if __name__ == "__main__":
-    #Generate Challenge
-    chall_ct, chall_crypt = generate_challenge()
+    with open("challenge-data/7.txt", "r") as f:
+        from base64 import b64decode
+        ciphertext = b64decode("".join([x.strip() for x in f.readlines()]))
+        #Generate Challenge
+        chall_ct, chall_crypt = generate_challenge(ciphertext)
 
-    #Generate oracle
-    chall_oracle = generate_oracle(chall_crypt)
+        #Generate oracle
+        chall_oracle = generate_oracle(chall_crypt)
 
-    #Attack
-    chall_flag = attack(chall_ct, chall_oracle).decode("ascii")
-    print(chall_flag)
-    #Challenge status
-    print("--- CHALLENGE STATUS: COMPLETE ---")
+        #Attack
+        chall_flag = attack(chall_ct, chall_oracle).decode("ascii")
+        print(chall_flag)
+        #Challenge status
+        print("--- CHALLENGE STATUS: COMPLETE ---")
